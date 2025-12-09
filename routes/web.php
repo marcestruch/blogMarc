f1695f2f7343c8a34313601f6f94711d2b0fcba1
Marc <marcstruchmalonda@gmail.com>
2025-12-09 18:34:45 +0100

    Ejercicio 2 vistas y rutas

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inici');
 
 Route::get('/posts', function(){
     return "Llistat de posts";
-});
+})->name('posts_llistat');
+
+Route::get('/posts/{id}', function($id){
+    return "Fitxa del post {$id}";
+})->where('id', '[0-9]+')->name('posts_fitxa');
