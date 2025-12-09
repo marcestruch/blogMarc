c777cd060a23e22116b22dc777ab2edbdac3d675
Marc <marcstruchmalonda@gmail.com>
2025-12-09 19:25:00 +0100

    Ejercicio 4 vistas y rutas

---
+++ resources/views/inici.blade.php
@@ -0,0 +1,8 @@
+@extends('plantilla')
+
+@section('titulo', 'Inici')
+
+@section('contenido')
+    <h1>Pàgina d'inici</h1>
+    <p>Benvingut al blog</p>
+@endsection
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,15 @@
-<nav>
-    <a href="{{ route('inici') }}">Pàgina d'inici</a>
-    |
-    <a href="{{ route('posts_llistat') }}">Llistat de posts</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Blog</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('inici') }}">Pàgina d'inici</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('posts_llistat') }}">Llistat de posts</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
