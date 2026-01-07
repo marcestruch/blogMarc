abc1758f0668a1bbf9a0dab34372d57b60e7ac3f
Marc <marcstruchmalonda@gmail.com>
2026-01-07 16:48:12 +0100

    He creado el helper.php para la fecha

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titulo')</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
     @include('partials.nav')
     <main>
         @yield('contenido')
     </main>
+    <footer class="text-center mt-4">
+        <p>Data actual: {{ fechaActual('d/m/Y') }}</p>
+    </footer>
 </body>
+
 </html>
