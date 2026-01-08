267ca421403aa2f808da8305727935875f3316bf
Marc <marcstruchmalonda@gmail.com>
2026-01-08 14:08:38 +0100

    He añadido apartado de informacion en la pagina de inicio

--- resources/views/inici.blade.php
+++ resources/views/inici.blade.php
@@ -4,5 +4,30 @@
 
 @section('contenido')
     <h1>Pàgina d'inici</h1>
-    <p>Benvingut al blog</p>
+    <div class="card text-center">
+        <div class="card-header">
+            Benvingut al blog
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">
+                Informacio sobre la pagina
+            </h5>
+            <p class="card-text">
+                Esta pagina permet la publicacio de posts desde un conter ja registrat en la base de dades.
+            </p>
+            <h5 class="card-title">
+                Informacio per als no registrats
+            </h5>
+            <ol class="list-group list-group-numbered">
+                <li class="list-group-item">
+                    No tens conter aixina que soles podras vore els posts creats per els usuaris reals
+                </li>
+                <li class="list-group-item">
+                    Si vols un conter tens que contactar amb el administrador
+                </li>
+            </ol>
+        </div>
+    </div>
+    
+
 @endsection
